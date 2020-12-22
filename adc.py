74c26605811dd402b48ab545faf1160645fe7267
Pasises <45708319+pasises@users.noreply.github.com>
2020-12-22 22:37:23 +0900

    feat : 4test20201223

--- adc.py
+++ adc.py
@@ -4,57 +4,59 @@ import pigpio
 # MCP3208から値を取得するクラス
 
 
-class MCP3208_Class:
+class ADC:
     channel = 1
     baud = 50000
     flags = 0
 
-    """コンストラクタ"""
-
     def __init__(self, pi, ref_volts):
+        """Initialize Motor Class
+        Args:
+            pi: 
+            ref_volts (float): Reference Volt of MCP3208
+        """
         self.pi = pi
         self.ref_volts = ref_volts
         self.h = pi.spi_open(self.channel, self.baud, self.flags)
 
-    """電圧取得"""
-
     def GetVoltage(self, ch):
+        """Get Voltage
+        Args:
+            ch (int): Channel of MCP3208
+        """
         #c, raw = self.pi.spi_xfer(self.h,[0x6,(8+ch)<<4,0])
         #c, raw = self.pi.spi_xfer(self.h,[0x6,ch<<6,0])
         c, raw = self.pi.spi_xfer(self.h, [1, (8 + ch) << 4, 0])
-        print("c: {0} raw: {1}".format(c, raw))
+        #print("c: {0} raw: {1}".format(c, raw))
         raw2 = ((raw[1] & 3) << 8) + raw[2]
         volts = (raw2 * self.ref_volts) / float(1023)
         volts = round(volts, 4)
         return volts
 
-    """終了処理"""
-
     def Cleanup(self):
+        """Cleanup
+        """
         self.pi.spi_close(self.h)
 
 
-"""メイン関数"""
 if __name__ == '__main__':
     pi = pigpio.pi()
 
     if not pi.connected:
         exit(0)
 
-    ADC = MCP3208_Class(pi, ref_volts=3.3)
+    adc = ADC(pi, ref_volts=3.3)
     try:
         while True:
-            volts = ADC.GetVoltage(ch=0)
+            volts = adc.GetVoltage(ch=0)
             print("volts ch0: {:8.2f}".format(volts))
-            #volts = ADC.GetVoltage(ch=1)
-            #print("volts ch1: {:8.2f}".format(volts))
-            #volts = ADC.GetVoltage(ch=2)
-            #print("volts ch2: {:8.2f}".format(volts))
+            volts = ADC.GetVoltage(ch=1)
+            print("volts ch1: {:8.2f}".format(volts))
             time.sleep(1)
     except KeyboardInterrupt:  # Ctl+Cが押されたらループを終了
         print("\nCtl+C")
     except Exception as e:
         print(str(e))
     finally:
-        ADC.Cleanup()
+        adc.Cleanup()
         print("\nexit program")
--- test20201223.py
+++ test20201223.py
@@ -1,15 +1,39 @@
 import time
 import datetime
 from log import*
-#from dac import*
-#from motor import*
+from adc import*
+from motor import*
 
 if __name__ == "__main__":
-    logname = fileName("log/log", "txt")
-    now = datetime.datetime.fromtimestamp(time.time())
-    saveLog(logname, logname, now.strftime('%Y/%m/%d %H:%M:%S'))
+    pi = pigpio.pi()
+    if not pi.connected:
+        exit(0)
+    adc = ADC(pi, ref_volts=3.3)
+    motor = Motor(18, 19)
 
-    for i in range(100):
-        saveLog(logname, time.time(), i)
-        time.sleep(0.1)
-        print(i)
+    try:
+        #--- Initialization ---#
+        logName = fileName("log/log", "txt")
+        now = datetime.datetime.fromtimestamp(time.time())
+        saveLog(logName, logname, now.strftime('%Y/%m/%d %H:%M:%S'))
+
+        #--- Main Process ---#
+        for i in range(100):
+            volt74 = adc.GetVoltage(ch=0) * 1.0
+            volt37 = adc.GetVoltage(ch=1) * 1.0
+            saveLog(logName, time.time(), i, volt74, volt37)
+            time.sleep(0.1)
+            print(i)
+
+    except KeyboardInterrupt:
+        print("\nKeyboard Interrupt")
+    except:
+        print(traceback.format_exc())
+        errorLogName = fileName("log/errorLog", "txt")
+        saveLog(errorLogName, time.time(), "Error")
+        saveLog(errorLogName, traceback.format_exc())
+        saveLog(errorLogName, "\n")
+    finally:
+        motor.stop()
+        adc.Cleanup()
+        print("\nexit program")
